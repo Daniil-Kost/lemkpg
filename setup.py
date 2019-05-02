5b1f33bee9c60efe37ca716b3e24a85ed357a6e9
Daniil <dkosty@softserveinc.com>
2019-05-02 14:34:13 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='lemkpg',
-      version='1.7',
+      version='1.8',
       description='lemkpg package give simple API interface for quick access to PostgreSQL DB via sync or async way',
       url='https://github.com/Daniil-Kost/lemkpg',
       author='Daniil Kostyshak',
